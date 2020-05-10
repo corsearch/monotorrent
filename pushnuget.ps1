7889e15bb0c6797d959f0bc386cacb7f3c5a9319
Gopal A <gopala000@gmail.com>
2020-05-10 14:11:32 -0700

    Try with nuget.exe

--- pushnuget.ps1
+++ pushnuget.ps1
@@ -3,5 +3,5 @@ set-location $args[0]
 $files=get-childitem | where {$_.Name -like "*.nupkg" -and $_.Name -notlike "*symbols*"}
 
 foreach($file in $files) {
-  dotnet nuget push $file.name --skip-duplicate --source "https://nuget.pkg.github.com/marketly/index.json" -k $args[1]
+  nuget push $file.name -SkipDuplicate -Source "https://nuget.pkg.github.com/marketly/index.json" -ApiKey $args[1]
 }
