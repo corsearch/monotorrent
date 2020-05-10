ad550c6c255ffd6b7ec160b1087116c25563e48a
Gopal A <gopala000@gmail.com>
2020-05-10 08:39:57 -0700

    echo new version #

--- incrversion.ps1
+++ incrversion.ps1
@@ -2,3 +2,5 @@ $file = ".\version.txt"
 $fileVersion = (Get-Content $file | Select -First 1).Split(".")
 $fileVersion[2] = [int]$fileVersion[2] + 1
 $fileVersion -join "." | Set-Content $file
+echo "New version number:"
+Get-Content $file
