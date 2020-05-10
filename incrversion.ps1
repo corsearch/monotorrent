3f030c5fa81fd7e01a8e5961cd29f1027fbabd8a
Gopal A <gopala000@gmail.com>
2020-05-10 09:22:14 -0700

    Pass version number between steps

--- incrversion.ps1
+++ incrversion.ps1
@@ -1,6 +1,20 @@
 $file = ".\version.txt"
+
+# get version number from file
 $fileVersion = (Get-Content $file | Select -First 1).Split(".")
+
+# incr version number
 $fileVersion[2] = [int]$fileVersion[2] + 1
-$fileVersion -join "." | Set-Content $file
+
+# form new version number
+$newFileVersion = $fileVersion -join "."
+
+# write new version number to file
+echo $newFileVersion | Set-Content $file
+
+# set version number in workflow variable in GitHub actions
+echo "::set-output name=version::$newFileVersion"
+
+# display file content
 echo "New version number:"
 Get-Content $file
