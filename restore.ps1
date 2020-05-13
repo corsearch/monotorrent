852b7991a10f1de9b1b651ad130f43378d7dc352
Gopal A <gopala000@gmail.com>
2020-05-13 10:14:36 -0700

    fix echo string

--- restore.ps1
+++ restore.ps1
@@ -3,6 +3,6 @@ set-location $args[0]
 $files=get-childitem | where {$_.Name -like "*.sln"}
 
 foreach($file in $files) {
-  echo "nuget restoring $file.name ..."
+  echo "nuget restoring $file ..."
   nuget restore $file.name
 }
